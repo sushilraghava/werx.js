edb3c07b61d8021f5e45eb92ef27c776a310b5b0
TrisKnightGamer <trihd123@gmail.com>
2022-10-24 23:53:20 +0700

    Update: L0, L1, BE Url

--- packages/werx-network/src/DNC.ts
+++ packages/werx-network/src/DNC.ts
@@ -1,6 +1,6 @@
 export class DNC {
-  static BLOCK_EXPLORER_URL = 'http://54.235.210.75:2050';
-  static L0_URL = 'http://20.249.7.13:9000';
-  static L1_URL = 'http://20.194.5.191:9000';
+  static BLOCK_EXPLORER_URL = 'https://54.235.210.75:2050';
+  static L0_URL = 'https://18.118.120.76:9000';
+  static L1_URL = 'https://18.118.120.76:9010';
 }
 
--- packages/werx-wallet/src/network-config.ts
+++ packages/werx-wallet/src/network-config.ts
@@ -2,14 +2,14 @@
 export const networkConfig = {
   2: {
     mainnet: {
-      beUrl: 'http://54.235.210.75:2050',
-      l0Url: 'http://3.14.19.8:9000',
-      l1Url: 'http://3.14.19.8:9010'
+      beUrl: 'https://54.235.210.75:2050',
+      l0Url: 'https://18.118.120.76:9000',
+      l1Url: 'https://18.118.120.76:9010'
     },
     testnet: {
-      beUrl: 'http://54.235.210.75:2050',
-      l0Url: 'http://3.14.19.8:9000',
-      l1Url: 'http://3.14.19.8:9010'
+      beUrl: 'https://54.235.210.75:2050',
+      l0Url: 'https://18.118.120.76:9000',
+      l1Url: 'https://18.118.120.76:9010'
     }
   }
 }
